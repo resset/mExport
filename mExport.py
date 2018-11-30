d590779db903f26655da0ed1ed3ac55af7d73ff5
Mateusz Tomaszkiewicz <ressetpublic@gmail.com>
2018-12-01 00:41:00 +0100

    Fix empty payee offer bug

--- mExport.py
+++ mExport.py
@@ -106,7 +106,7 @@ with open(BANK_DUMP_FILE) as f:
     # Further processing
     for i, entry in enumerate(entries):
         if not 'payee' in entry or '' == entry['payee']:
-            entries[i]['payee'] = entry['lines'][2]
+            entries[i]['payee'] = entry['lines'][1]
         if not 'mode' in entry:
             entries[i]['mode'] = ''
         if not 'category' in entry:
